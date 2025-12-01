f5d9e5c3b1ac12bae0f87ce24becbbdbcb0cba27
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-01 17:19:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
-# config.py
 import os
-
-TOKEN = os.environ.get("BOT_TOKEN", "YOUR_TOKEN_HERE")
-DB = "boobs.db"
-PHOTO_DIR = "photos"
-DONATE_PRICE = 10
-ADMIN_USERNAME = "Sugar_Daddy_rip"
+TOKEN = os.environ.get("BOT_TOKEN", "ЗДЕСЬ_ТВОЙ_ТОКЕН")
