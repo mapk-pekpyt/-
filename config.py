d3748bcac3e08d9b61c0507ffcd70e3176796289
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-01 17:35:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+# config.py
 import os
-TOKEN = os.environ.get("BOT_TOKEN", "ЗДЕСЬ_ТВОЙ_ТОКЕН")
+
+TOKEN = os.environ.get("BOT_TOKEN", "YOUR_TOKEN_HERE")
+PROVIDER_TOKEN = os.environ.get("PROVIDER_TOKEN", "")  # если есть подключенный провайдер
+DB_PATH = os.environ.get("DB_PATH", "boobs.db")
+PHOTO_DIR = os.environ.get("PHOTO_DIR", "photos")
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Sugar_Daddy_rip")
